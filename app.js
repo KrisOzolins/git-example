496092737b68c0e522eba15776cfa11522259136
Krisjanis Ozolins <kris.ozolins@gmail.com>
2018-11-03 13:18:45 +0200

    Added class example

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-console.log(12345);
-
 // Fast way to check if list, or in this case array,
 // contains two values which added are equal to the provided sum
 var hasPairWithSum = function (data, sum) {
     var comp = [];
     var hasFound = false;
     data.some((element, index) => {
-        console.log(index);
         if (comp.indexOf(element) != -1) {
             hasFound = true;
             return true;
@@ -21,3 +18,35 @@ console.log(hasPairWithSum([1, 2, 3, 4], 8));
 
 // Should return true
 console.log(hasPairWithSum([1, 4, 4, 2], 8));
+
+class Animal {
+    constructor(name, age, type) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+    }
+
+    getAgeSentence() {
+        return `${this.name} is ${this.age} years old.`;
+    }
+
+    static getTypes() {
+        return {
+            dog: 1,
+            cat: 2,
+            cat: 3,
+        }
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, age, tag) {
+        super(name,age, Animal.getTypes().dog)
+        this.tag = tag;
+    }
+}
+
+const dog1 = new Animal('Rex', '7', Animal.getTypes().dog);
+console.log(dog1);
+const dog2 = new Dog('Rex', '7', 'Tag name');
+console.log(dog2);
